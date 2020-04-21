c803ecf8379c640daee0d188eba90798332b71d3
cucobein <cucobein@gmail.com>
2020-04-21 14:45:52 -0500

    Added 2Y0A02 sensors

--- mycros-one.ino
+++ mycros-one.ino
@@ -1,24 +1,25 @@
 
-#define SWITCH_IN 2
-#define SWITCH_OUT 3
-#define PUMP 13
-#define DEBOUNCE_DELAY 50 // Miliseconds
-#define PUMP_TIME 3 // Seconds
+#define SWITCH_IN         2
+#define SWITCH_OUT        3
+#define PUMP              13
+#define DEBOUNCE_DELAY    50    // Miliseconds
+#define PUMP_TIME         5     // Seconds
 #define IDLE_STATE        0
 #define PUMP_OPEN_STATE   1
+#define N_SAMPLES         128
+#define THRESHOLD_VALUE   0.5
 
-float timerValue = 62500; // 16MHz-256-1Hz
-float timerCount = 0;
-float pumpTimeCounter = 0;
-float currentState = IDLE_STATE;
+unsigned int timerValue = 62500;       // 16MHz-256-1Hz
+unsigned int timerCount = 0;
+unsigned int pumpTimeCounter = 0;
+unsigned char currentState = IDLE_STATE;
+double inValues[N_SAMPLES];
+double outValues[N_SAMPLES];
+unsigned int samplesIndex = 0;
 
 void setup() {
   pinMode(PUMP, OUTPUT);
-  pinMode(SWITCH_IN, INPUT_PULLUP);
-  pinMode(SWITCH_OUT, INPUT_PULLUP);
-  digitalWrite(LED_BUILTIN, LOW);
-  attachInterrupt(digitalPinToInterrupt(SWITCH_IN), switchInPressed, FALLING);
-  attachInterrupt(digitalPinToInterrupt(SWITCH_OUT), switchOutPressed, FALLING);
+  digitalWrite(PUMP, LOW);
 
   noInterrupts();
   TCCR1A = 0;
@@ -31,28 +32,8 @@ void setup() {
 
   Serial.begin(115200);
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Native USB only
+    ; // wait for serial port to connect
   }
-}
-
-void switchInPressed() {
-  noInterrupts();
-  delay(DEBOUNCE_DELAY);
-  if(digitalRead(SWITCH_IN) == LOW) {
-    startTimer();
-    currentState = PUMP_OPEN_STATE;
-  }
-  interrupts();
-}
-
-void switchOutPressed() {
-  noInterrupts();
-  delay(DEBOUNCE_DELAY);
-  if(digitalRead(SWITCH_OUT) == LOW) {
-    startTimer();
-    currentState = PUMP_OPEN_STATE;
-  }
-  interrupts();
 }
 
 void startTimer() {
@@ -79,12 +60,48 @@ void turnOffPump() {
   }
 }
 
+void sampleAnalogValues() {
+  double inValue = double(analogRead(A0) * 5.0 / 1024.0);
+  double outValue = double(analogRead(A1) * 5.0 / 1024.0);
+  if(samplesIndex < N_SAMPLES) {
+    inValues[samplesIndex] = inValue;
+    outValues[samplesIndex] = outValue;
+    samplesIndex++;
+  } else {
+    double inAverage = samplesAverage(inValues);
+    double outAverage = samplesAverage(outValues);
+    if(inAverage >= THRESHOLD_VALUE || outAverage >= THRESHOLD_VALUE) {
+      startTimer();
+      currentState = PUMP_OPEN_STATE;
+    } 
+    clearSamples();
+  }
+}
+
+double samplesAverage(double a[]) 
+{ 
+    double sum = 0; 
+    for (int i=0; i < N_SAMPLES; i++) 
+       sum += a[i]; 
+    return sum / double(N_SAMPLES); 
+} 
+
+void clearSamples() 
+{ 
+    for(int i=0; i<N_SAMPLES; i++) {
+      inValues[i] = 0;
+      outValues[i] = 0;
+    }
+    samplesIndex = 0;
+} 
+
 ISR(TIMER1_COMPA_vect)                   
 {
   pumpTimeCounter++;                     
 }
 
 void loop() {
+  sampleAnalogValues();
   if(currentState == IDLE_STATE) {
     turnOffPump();
   } else if(currentState == PUMP_OPEN_STATE) {
@@ -95,4 +112,5 @@ void loop() {
       turnOnPump();
     }
   }
+  delay(1);
 }
